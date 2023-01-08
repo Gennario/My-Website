e9c98fd0df89a71acabdba69110f504c7e909270
Gennario <48251906+Gennario@users.noreply.github.com>
2023-01-08 18:48:29 +0100

    some changes

--- code.js
+++ code.js
@@ -1,41 +1,86 @@
-const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor");
+// LANGUAGE
 
-const textArray = ["Graphic Designer", "Java Developer", "Discord user", "Web designer", "UX/UI Designer"];
-const typingDelay = 100;
-const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
-let textArrayIndex = 0;
-let charIndex = 0;
+var storage = window.localStorage;
+var request = new XMLHttpRequest();
+console.log(getLanguage());
+request.open("GET", "languages/"+getLanguage()+".json", false);
+request.send(null);
+const obj = JSON.parse(request.responseText);
 
-function type() {
-    if (charIndex < textArray[textArrayIndex].length) {
-        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-        charIndex++;
-        setTimeout(type, typingDelay);
-    }
-    else {
-        cursorSpan.classList.remove("typing");
-        setTimeout(erase, newTextDelay);
+function getByLanguage(value, element) {
+    document.getElementById(element).textContent = value;
+}
+
+function setLanguage(lang) {
+    if (typeof (storage) !== "undefined") {
+        storage.setItem("language", lang);
+    } else {
     }
 }
 
-function erase() {
-    if (charIndex > 0) {
-        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-        charIndex--;
-        setTimeout(erase, erasingDelay);
-    }
-    else {
-        cursorSpan.classList.remove("typing");
-        textArrayIndex++;
-        if(textArrayIndex>=textArray.length) textArrayIndex=0;
-        setTimeout(type, typingDelay);
+function getLanguage() {
+    if (storage.getItem("language") === null) {
+        setLanguage('english')
+        return 'english';
+    } else {
+        return storage.getItem("language");
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-    if(textArray.length) setTimeout(type, 250);
-});
+// DROPDOWN
+
+function toggleDropdown1() {
+    document.getElementById("dropdown_content").classList.toggle("show");
+    if (document.getElementById("dropdown_content").classList.contains("show")) {
+        document.getElementById("dropdown-arrow").style.transform = 'rotate(180deg)';
+    }else {
+        document.getElementById("dropdown-arrow").style.transform = '';
+    }
+}
+
+// scroll animation
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 50;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("activereveal");
+        } else {
+            reveals[i].classList.remove("activereveal");
+        }
+
+        if(!isNaN(reveals[i].textContent)) {
+            incEltNbr(reveals[i].id);
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+// animated number
+
+var speed = 20;
+
+/* Call this function with a string containing the ID name to
+ * the element containing the number you want to do a count animation on.*/
+function incEltNbr(id) {
+    elt = document.getElementById(id);
+    endNbr = Number(document.getElementById(id).innerHTML);
+    incNbrRec(0, endNbr, elt);
+}
+
+/*A recursive function to increase the number.*/
+function incNbrRec(i, endNbr, elt) {
+    if (i <= endNbr) {
+        elt.innerHTML = i + '+';
+        setTimeout(function() {//Delay a bit before calling the function again.
+            incNbrRec(i + 1, endNbr, elt);
+        }, speed);
+    }
+}
